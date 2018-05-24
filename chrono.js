4ce8a4c8bf52dba8ed26e16e67c173ae675a373f
david <david.journo.31@gmail.com>
2018-05-25 00:07:27 +0200

    changes to start

--- chrono.js
+++ chrono.js
@@ -1,22 +1,18 @@
 function Chrono() {
     let minutes = 0;
     let secondes = 0;
-    let dixiemes = 0;
+    let milsec = 0;
     this.start = function () {
         dixiemes++
-        if (dixiemes > 9) {
-            dixiemes = 0;
+        if (milsec > 9) {
+            milsec = 0;
             secondes++
         }
         if (secondes > 59) {
             secondes = 0;
             minutes++
         }
-        document.forsec.mil.value = " " + dixiemes //on affiche les dixièmes
-        document.forsec.sec.value = " " + secondes //on affiche les secondes
-        document.forsec.min.value = " " + minutes //on affiche les minutes
-        compte = setTimeout('chrono()', 100) //la fonction est relancéetous les 10° de secondes
-        console.log(dixiemes)
+        console.log(milsec)
     }
     this.pause = function () {
 
